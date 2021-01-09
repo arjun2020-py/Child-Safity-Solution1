3619cd006478524ea6637cd4cd25e055a32c81ea
arjun <arjunkoodathil@gmail.com>
2021-01-09 20:19:59 +0530

    chatroom ok

--- inbox.js
+++ inbox.js
@@ -1,28 +1,32 @@
-let saftyPassword = "mptc123";
+let saftyPasswrod = "mptc123";
+
 const chatroom_button = document.getElementById("chatbox_button");
-chatroom_button.addEventListener("click",askPassword);
+if(chatroom_button)
+{
+chatroom_button.addEventListener("click", askPassword);
+}
 const passwordInput = document.querySelector("#password");
 function getPassword() {
   saftyPassword = passwordInput.value;
 
-  if (saftyPassword){
+  if (saftyPassword) {
     alert("Password successfully saved");
     let link = document.createElement("a")
-   link.href = "./parent.html";
+    link.href = "./parent.html";
     link.click();
 
   }
-  else{
+  else {
     alert("acess deined")
   }
-  
+
 }
 
 function askPassword(e) {
-e.preventDefault();
+  e.preventDefault();
 
-let password = prompt("Give me the password");
-  if (saftyPasswrod ===password) {
+  let password = prompt("Give me the password");
+  if (saftyPasswrod === password) {
     let link = document.createElement("a")
     link.href = "./chatroom.html";
     link.click();
