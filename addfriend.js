631961ebad93016311fc3954d257edbeebca1da9
arjun <arjunkoodathil@gmail.com>
2021-01-10 20:51:20 +0530

    councling

--- addfriend.js
+++ addfriend.js
@@ -1,5 +1,8 @@
+
 function addFriend(e) {
+    /*dont use button agin*/
     e.disabled = true;
+            /* transpernt style*/
     e.style.opacity = .8;
     e.innerHTML = "Added"
   }
---
+++ conculing.js
@@ -0,0 +1,7 @@
+let link1=document.createElement("a");
+function forgoton_button(e){
+    alert("conculingpage")
+   e.preventDefault();
+    link1.href="./otp.html";
+    link1.click();
+    }
