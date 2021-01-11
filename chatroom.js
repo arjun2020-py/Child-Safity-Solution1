fb2c76154bd6a796efb0f960abf43f717263ddca
arjun <arjunkoodathil@gmail.com>
2021-01-11 19:11:34 +0530

    chatroom room add some features

--- chatroom.js
+++ chatroom.js
@@ -1,16 +1,18 @@
-const messageBox = document.querySelector(".outgoing");
+const outgoing = document.querySelector(".outgoing");
+const incomming = document.querySelector(".incoming");
 const input = document.querySelector("input");
+const currentUser = document.querySelector("#currentUser");
 
 const sendMessage = () => {
   if (input.value.trim() !== "") {
     const messageDiv = document.createElement("div");
     messageDiv.classList.add("bubble");
     messageDiv.innerHTML = input.value;
-    messageBox.appendChild(messageDiv);
+    outgoing.appendChild(messageDiv);
     input.value = "";
   }
 }
-
+  /*list persion item cliock item send to 'e'*/ 
 const toggleClass = (e) => {
   const persons = document.getElementsByClassName("person");
 
@@ -19,4 +21,13 @@ const toggleClass = (e) => {
   }
 
   e.classList.add("focus");
+  /*elelment content change or data get or set use innerHtml*/ 
+  const messageDiv = document.createElement("div");
+  currentUser.innerHTML = e.childNodes[1].innerHTML;
+  messageDiv.classList.add("bubble");
+  messageDiv.innerHTML = e.childNodes[6].innerHTML;
+  /*incmoing & outgoing is clear chyan */
+  incomming.innerHTML="";
+   outgoing.innerHTML=" ";
+  incomming.appendChild(messageDiv)
 }
