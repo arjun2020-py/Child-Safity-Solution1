df8aa6468e66f6ac09b0382a1d5ce7331cc1e29e
arjun <arjunkoodathil@gmail.com>
2021-01-12 22:04:41 +0530

    add splash screen in index webpage

---
+++ demo.js
@@ -0,0 +1,33 @@
+let intro =  document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logospan =document.querySelectorAll('.logo');
+window.addEventListener('load',()=>{
+   setTimeout(() => {
+      
+    logospan.forEach((span,idx)=>{
+
+        setTimeout(() => {
+            
+            span.classList.add('active');
+        }, [idx + 1]+400);
+    });
+
+setTimeout(() => {
+    
+    logospan.forEach((span,idx)=>{
+        setTimeout(() => {
+           
+            span.classList.remove('active');
+            span.classList.add('fade');
+  
+        },[idx + 1]*50);
+    })
+},5000);
+
+setTimeout(() => {
+    intro.style.top = '-100vh';
+},5000);
+
+   },)
+})
+
