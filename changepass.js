6c65bd10ab6e0f43ddf61c87687faf1c7f70ff93
arjun <arjunkoodathil@gmail.com>
2021-01-13 23:00:27 +0530

    add change passwroad in setting page

---
+++ changepass.js
@@ -0,0 +1,10 @@
+let link1=document.createElement("a");
+function forgoton_button(e){
+    alert("page change")
+   // e.preventDefault();
+    link1.href="./otp.html";
+    link1.click();
+    }
+    
+
+ 
