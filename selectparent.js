eba4b9b4604dbf15d62bec5543c6e628b405d834
arjun <arjunkoodathil@gmail.com>
2021-01-19 23:41:39 +0530

    add changes in saftypasswrod occor an  error

--- selectparent.js
+++ selectparent.js
@@ -1,4 +1,9 @@
-function GotoHome(){
+const form=document.getElementById("form1");
+
+form.addEventListener("submit",GotoHome)
+
+function GotoHome(e){
+    e.preventDefault();
     let link=document.createElement("a");
     link.href='./child.html';
     link.click();
