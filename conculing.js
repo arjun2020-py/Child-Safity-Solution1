a61e8fac6ce2f0b5eec5f7c7394064acc13eda14
Yedhu <gyedhumohanan@gmail.com>
2021-01-23 21:00:50 +0530

    counsiling page resonsive

--- conculing.js
+++ conculing.js
@@ -1,7 +1,6 @@
-let link1=document.createElement("a");
-function forgoton_button(e){
+function forgoton_button(e) {
     alert("conculingpage")
-   e.preventDefault();
-    link1.href="./otp.html";
+    e.preventDefault();
+    link1.href = "./otp.html";
     link1.click();
-    }
+}
