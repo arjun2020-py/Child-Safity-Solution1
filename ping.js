1747e3ef1cd795baf7d3527ecdb1bc2fe176926a
arjun <arjunkoodathil@gmail.com>
2021-01-23 21:53:59 +0530

    add change sing page

---
+++ ping.js
@@ -0,0 +1,10 @@
+ const map=document.getElementById('map')
+ function showmap(){
+     const result= confirm ('give our permission');
+     if(result==true){
+         /**/
+         map.style.display='block';
+     }
+    
+
+ }
